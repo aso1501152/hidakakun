d87c51c34ab58ffeb3703c63400b04b5038a9ade
aso1501133 <1501133@st.asojuku.ac.jp>
2017-05-26 12:05:13 +0900

    hidaka

--- app/src/main/java/jp/ac/asojuku/jousisenb/hidakakun/yabumoto.java
+++ app/src/main/java/jp/ac/asojuku/jousisenb/hidakakun/yabumoto.java
@@ -9,5 +9,6 @@ public class yabumoto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yabumoto);
+        int enndaaaaaaaaaaaaaaaaaaa;
     }
 }
