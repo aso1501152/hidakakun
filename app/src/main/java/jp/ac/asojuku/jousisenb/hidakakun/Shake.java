72246eee46e49db39985c3e7e9239bd9c5dc920d
tabirahiromu <1501123@st.asojuku.ac.jp>
2017-06-02 12:50:54 +0900

    rwfr

--- app/src/main/java/jp/ac/asojuku/jousisenb/hidakakun/Shake.java
+++ app/src/main/java/jp/ac/asojuku/jousisenb/hidakakun/Shake.java
@@ -5,12 +5,43 @@ package jp.ac.asojuku.jousisenb.hidakakun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
 
-public class Shake extends AppCompatActivity {
+public class Shake extends AppCompatActivity implements SensorListener {
+
+    private static final int FORCE_THRESHOLD = 350;
+    private static final int TIME_THRESHOLD = 100;
+    private static final int SHAKE_TIMEOUT = 500;
+    private static final int SHAKE_DURATION = 100;
+    private static final int SHAKE_COUNT = 3;
+
+    private SensorManager mSensorManager;
+    private float mLastX = -1.0f, mLastY = -1.0f, mLastZ = -1.0f;
+    private long mLastTime;
+    private OnShakeListener mShakeListener;
+    private Context mContext;
+    private int mShakeCount = 0;
+    private long mLastShake;
+    private long mLastForce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shake);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_hoge, menu);
+        return true;
+    }
+
+    @Override
+    public void onAccuracyChanged(int sensor, int accuracy) {
+
     }
 }
