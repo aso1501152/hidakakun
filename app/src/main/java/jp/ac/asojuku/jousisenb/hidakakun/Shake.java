cddd011cdd7b3e47ce4e7a9f6328c7fab6c729f3
tabirahiromu <1501123@st.asojuku.ac.jp>
2017-06-23 12:39:27 +0900

    rwfr

--- app/src/main/java/jp/ac/asojuku/jousisenb/hidakakun/Shake.java
+++ app/src/main/java/jp/ac/asojuku/jousisenb/hidakakun/Shake.java
@@ -3,11 +3,11 @@
  */
 package jp.ac.asojuku.jousisenb.hidakakun;
 
+import android.media.UnsupportedSchemeException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.view.Menu;
@@ -36,12 +36,79 @@ public class Shake extends AppCompatActivity implements SensorListener {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_hoge, menu);
+            getMenuInflater().inflate(R.menu.activity_hoge, menu);
         return true;
     }
 
     @Override
     public void onAccuracyChanged(int sensor, int accuracy) {
 
+    }
+
+    @Override
+    public void onSensorChanged(int sensor, float[] values) {
+        if(sensor != SensorManager.SENSOR_ACCELッバEROMETER) return;
+
+        long now = System.currentTimeMillis();
+
+        if ((now - mLastForce) > SHAKE_TIMEOUT){
+            mShakeCount = 0;
+        }
+         if ((now - mLastTime) > TIME_THRESHOLD){
+             long diff = now - mLastTime;
+
+             float speed = Math.abs(values[SensorManager.DATA_X] +
+                     values[SensorManager.DATA_Y] +
+                     values[SensorManager.DATA_Z] -
+                     mLastX - mLastY -mLastZ ) / diff * 10000;
+
+             if ( speed > FORCE_THRESHOLD ){
+                 if ( ( ++mShakeCount >= SHAKE_COUNT ) && now - mLastShake > SHAKE_DURATION) {
+                     mLastShake = now;
+                     mShakeCount = 0;
+                     if ( mShakeListener != null ){
+                         mShakeListener.onShake();
+                     }
+                 }
+                 mLastForce = now;
+             }
+             mLastTime = now;
+             mLastX = values[SensorManager.DATA_X];
+             mLastY = values[SensorManager.DATA_Y];
+             mLastZ = values[SensorManager.DATA_Z];
+         }
+    }
+
+    public interface OnShakeListener {
+        public void onShake();
+    }
+
+    public ShakeListener (Context context) {
+        mContext = context;
+        resume();
+    }
+
+    public void setOnShakeListener ( OnShakeListener listener ){
+        mShakeListener = listener;
+    }
+
+    public void resume() {
+        mSensorManager = (SensorManager) mContext.getSystemService(SEARCH_SERVICE);
+        if( mSensorManager == null){
+            throw new UnsupportedOperationException("Sensor not suported");
+        }
+        boolean supportd = mSensorManager.registerListener(this,
+                SensorManager.SENSOR_ACCELEROMETER,
+                SensorManager.SENSOR_DELAY_GAME);
+        if (!supportd ){
+            mSensorManager.unregisterListener(this, SensorManager.SENSOR_ACCELEROMETER);
+            throw new UnsupportedOperationException("Acceleroneter not supported");
+        }
+
+        public void pause(){
+        if (mSensorManager != null){
+            mSensorManager.unregisterListener(this, SensorManager.SENSOR_ACCELEROMETER);
+            mSensorManager = null;
+        }
     }
 }
